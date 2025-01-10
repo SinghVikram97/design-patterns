ff94dc874bf097c566d1510e4698d2e381861e80
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2025-01-09 19:27:06 -0800

    implemented prototype

---
+++ src/main/java/org/vikram/prototype/Main.java
@@ -0,0 +1,41 @@
+package org.vikram.prototype;
+
+interface Prototype{
+    Prototype cloneObj();
+}
+
+class Student implements Prototype{
+    private final String name;
+    private final int rollNo;
+
+    private final int age;
+
+    public Student(String name, int rollNo, int age) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNo=" + rollNo +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public Prototype cloneObj() {
+        return new Student(name, rollNo, age);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Student originalObj = new Student("Vikram", 12, 26);
+
+        Student cloneObj = (Student) originalObj.cloneObj();
+
+        System.out.println(cloneObj);
+    }
+}
